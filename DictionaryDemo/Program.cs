d148e67d0a4dab70d8c2a9ecf1b1391b592eb09b
veli eski <veli.eski.20@gmail.com>
2021-01-23 20:21:34 +0300

    Add project files.

---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> plakalar = new MyDictionary<int, string>();
+            plakalar.Add(1, "Adana");
+            Console.WriteLine("Adana ili eklendi");
+            
+            plakalar.Add(2, "Adıyaman");
+            Console.WriteLine("Adıyaman ili eklendi");
+
+            plakalar.List();
+
+            
+
+        }
+
+    }
+}
